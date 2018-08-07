7ab23e355c943b5b4c1a99bb9ca74081fb95d511
chowal <chowal@oregonstate.edu>
2018-08-07 15:40:24 -0700

    Possible example for the first partition test?

--- URLValidatorInCorrect/test/UrlValidatorTest.java
+++ URLValidatorInCorrect/test/UrlValidatorTest.java
@@ -22,23 +22,36 @@ public class UrlValidatorTest extends TestCase {
    public void testManualTest()
    {
 //You can use this function to implement your manual testing	   
-	   UrlValidator urlVal = new UrlValidator(null, null, UrlValidator.ALLOW_ALL_SCHEMES);
+	   UrlValidator urlVal = new UrlValidator(UrlValidator.ALLOW_ALL_SCHEMES);
 	   // MANUAL TEST 1
-	   System.out.printf("Testing http://www.google.com%n");
-	   boolean res =  urlVal.isValid("http://www.google.com");
+	   String test1 = "http://www.google.com";
+	   System.out.printf("Testing %s%n", test1);
+	   boolean res =  urlVal.isValid(test1);
 	   if (res) {
 		   System.out.printf("PASS: %b%n", res); 
 	   } else {
 		   System.out.printf("FAIL: %b%n", res);
 	   }
 	   assertTrue(res);
+	   
+	   assertTrue(urlVal.isValid("http://www.google.com/index.html"));
+	   //assertTrue(urlVal.isValid("http://www.google.com:80/test/index.html"));
+	   //assertTrue(urlVal.isValid("http://google.com///test1"));
    }
    
-   
+  
+   /*
+    *  the first partition: "www" of www.google.com
+    * 
+    */
    public void testYourFirstPartition()
    {
 	 //You can use this function to implement your First Partition testing	   
-
+	  UrlValidator urlVal = new UrlValidator(UrlValidator.ALLOW_ALL_SCHEMES);
+	  assertTrue(urlVal.isValid("http://www.google.com"));
+	  assertTrue(urlVal.isValid("http://.google.com"));
+	  assertTrue(urlVal.isValid("http://google.com"));
+	  assertTrue(urlVal.isValid("http://23r2.google.com"));
    }
    
    public void testYourSecondPartition(){
@@ -56,5 +69,6 @@ public class UrlValidatorTest extends TestCase {
    public static void main(String[] argv) {
 		  UrlValidatorTest fct = new UrlValidatorTest("url test");
 	      fct.testManualTest();
+	      fct.testYourFirstPartition();
 	   } 
 }
