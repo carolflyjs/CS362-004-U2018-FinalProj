77056c3dc13837ff82bb0ef2baea49439c476d34
chowal <chowal@oregonstate.edu>
2018-08-09 15:51:56 -0700

    added manual tests and incomplete first partition

--- URLValidatorInCorrect/test/UrlValidatorTest.java
+++ URLValidatorInCorrect/test/UrlValidatorTest.java
@@ -1,5 +1,3 @@
-
-
 import static org.junit.Assert.assertEquals;
 
 import junit.framework.TestCase;
@@ -8,9 +6,7 @@ import junit.framework.TestCase;
 //It is an optional to use this file, you can generate your own test file(s) to test the target function!
 // Again, it is up to you to use this file or not!
 
-
-
-
+// Partition source: https://en.wikipedia.org/wiki/URL
 
 public class UrlValidatorTest extends TestCase {
 
@@ -20,58 +16,73 @@ public class UrlValidatorTest extends TestCase {
    }
 
    
-   
+   /*
+    * Testing: Manual tests
+    * 
+    */
    public void testManualTest()
    {
-//You can use this function to implement your manual testing	   
+	   System.out.println("-----Manual Tests -----");
 	   UrlValidator urlVal = new UrlValidator(UrlValidator.ALLOW_ALL_SCHEMES);
-	   
-	   //The first parameter is the EXPECTED value and we are asserting it against
-	   //the returned value from isValid().
-	   assertEquals(true, urlVal.isValid("http://www.google.com"));
-	   assertEquals(false, urlVal.isValid("http://www.google.com:80/test/index.html"));
-	   assertEquals(false, urlVal.isValid("http://google.com///test1"));
-	   
+
+	   String manualTrue[] = {"http://www.google.com",
+			   				  "http://www.com",
+			   				  "http://www.google.com:80/test/index.html/"
+	   };
+	   String manualFalse[] = {"httwwgoocom",
+			   				  "12315",
+			   				  "http://google.com///test1",
+			   				  "C:/Programs/index.html",
+	   };
+
+	   System.out.println("Expected: True");
+	   for(int i = 0; i < manualTrue.length; i++) {
+		   System.out.printf("\"%s\" -- Result: ", manualTrue[i]);
+		   System.out.printf("%b%n", urlVal.isValid(manualTrue[i]));
+		   System.out.flush();
+	   }
+	   System.out.println("Expected: False");
+	   for(int j = 0; j < manualFalse.length; j++) {
+		   System.out.printf("\"%s\" -- Result: ", manualFalse[j]);
+		   System.out.printf("%b%n", urlVal.isValid(manualFalse[j]));
+		   System.out.flush();
+	   }
+
    }
    
-   
+   /*
+    * Testing: AUTHORITY
+    * 
+    */
    public void testYourFirstPartition()
    {
-	 //You can use this function to implement your First Partition testing
+	   System.out.println("-----First Parition: Authority-----");
 	   UrlValidator urlVal = new UrlValidator(UrlValidator.ALLOW_ALL_SCHEMES);
 	   
-	   //The first partition tests the AUTHORITY validation of isValid().
-	   assertEquals(true, urlVal.isValid("http://www.google.com"));
-	   assertEquals(true, urlVal.isValid("http://.google.com"));
-	   assertEquals(true, urlVal.isValid("http://google.com"));
-	   //assertEquals(false, urlVal.isValid("http://23r2.google.com")); //bug?
-	   
+	   String authTrue[] = {"www.google.com",
+			   				"255.255.255.255",
+			   				"400.400.400.400",
+			   				"80"
+	   };
+	   for(int i = 0; i < authTrue.length; i++) {
+		   System.out.printf("\"%s\" -- Result: ", authTrue[i]);
+		   System.out.printf("%b%n", urlVal.isValid("http://" + authTrue[i]));
+		   System.out.flush();
+	   }
    }
    
-   public void testYourSecondPartition()
-   {
-	   //The second partition tests the SCHEME validation.
-	   UrlValidator urlVal = new UrlValidator(UrlValidator.ALLOW_ALL_SCHEMES);
-	   assertEquals(true, urlVal.isValid("http://www.google.com"));
-	   //assertEquals(true, urlVal.isValid("h3t://www.google.com")); //bug?
-	   //assertEquals(true, urlVal.isValid("www.google.com")); //bug?
-	   
-	   assertEquals(false, urlVal.isValid("3htp://www.google.com"));
-	   assertEquals(false, urlVal.isValid("http//www.google.com"));
-	   //assertEquals(false, urlVal.isValid("http:/www.google.com")); //bug?
-	   assertEquals(false, urlVal.isValid("http:www.google.com"));
-	   assertEquals(false, urlVal.isValid("://www.google.com"));
-   }
-   
-   /*public void testYourSecondPartition(){
+   /*
+    * Testing: SCHEME
+    * 
+    */
+   public void testYourSecondPartition(){
 		 //You can use this function to implement your Second Partition testing
 	   UrlValidator urlVal = new UrlValidator(null, null, UrlValidator.ALLOW_ALL_SCHEMES);
 	   
 	   //The second partition tests the SCHEME validation of isValide().
 	   String testString;
-	   String prefix = "";
-	   String[] trueSchemes = {"http://",
-			   					"h3t://", 
+	   
+	   String[] trueSchemes = {"http://", 
 			   					""};
 	   
 	   String[] falseSchemes = {"3htp://", 
@@ -82,6 +93,8 @@ public class UrlValidatorTest extends TestCase {
 	   
 	   String otherParts = "www.google.com";
 	   
+	   System.out.println();
+	   System.out.println("-----Second Partition: Schemes-----");
 	   
 	   //test isValid() with supposedly valid URL's
 	   for (int i = 0; i < trueSchemes.length; i++) {
@@ -89,23 +102,81 @@ public class UrlValidatorTest extends TestCase {
 		   testString = trueSchemes[i] + otherParts;
 		   
 		   boolean result = urlVal.isValid(testString);
-		   assertEquals(true, result);
+		   
+		   System.out.println("\"" + trueSchemes[i]+ "\"" + "--	Expected: true;	Actual Result: " + result);
 		   
 		   testString = "";
 	   }
 	   
 	   //test isValid() with supposedly invalid URL's
-	   for (int i = 0; i < falseSchemes.length; i++) {
+	   for (int j = 0; j < falseSchemes.length; j++) {
 		   
-		   testString = prefix + falseSchemes[i] + otherParts;
+		   testString = falseSchemes[j] + otherParts;
 		   
-		   assertEquals(false, urlVal.isValid(testString));
+		   boolean result = urlVal.isValid(testString);
+		   
+		   System.out.println("\"" + falseSchemes[j]+ "\"" + "--	Expected: false;	Actual Result: " + result);
 		   
 		   testString = "";
 	   }
 	   
-   }*/
-   //You need to create more test cases for your Partitions if you need to 
+   }
+
+   /*
+    * Testing: PATH 
+    * 
+    */
+   public void testYourThirdPartition(){
+	   UrlValidator urlVal = new UrlValidator(null, null, UrlValidator.ALLOW_ALL_SCHEMES);
+	   
+	   //The second partition tests the PATH validation of isValid().
+	   String testString;
+	   String prefix = "http://www.google.com";
+	   
+	   String[] truePath = {"", 
+			   				"/test1", 
+			   				"/test1/", 
+			   				"/t123", 
+			   				"/123", 
+			   				"/$23"};
+	   
+	   String[] falsePath = {"/..", 
+			   					"/../", 
+			   					"//.", 
+			   					"//.//", 
+			   					"/test1//file", 
+			   					"//test/file"};
+	   
+	   
+	   System.out.println();
+	   System.out.println("-----Third Partition: Path-----");
+	   
+	   //test isValid() with supposedly valid URL's
+	   for (int i = 0; i < truePath.length; i++) {
+		   
+		   testString = prefix + truePath[i];
+		   
+		   boolean result = urlVal.isValid(testString);
+		   
+		   System.out.println("\"" + testString + "\"" + "--	Expected: true;	Actual Result: " + result);
+		   
+		   testString = "";
+	   }
+	   
+	   //test isValid() with supposedly invalid URL's
+	   for (int j = 0; j < falsePath.length; j++) {
+		   
+		   testString = prefix + falsePath[j];
+		   
+		   boolean result = urlVal.isValid(testString);
+		   
+		   System.out.println("\"" + testString + "\"" + "--	Expected: false;	Actual Result: " + result);
+		   
+		   testString = "";
+	   }
+	   
+ }
+   
    
    public void testIsValid()
    {
